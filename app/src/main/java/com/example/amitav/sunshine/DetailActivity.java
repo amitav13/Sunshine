0c38e0239305818f1ced4b0845f49f96f4ebfa60
amitav13 <amitav.khandelwal@gmail.com>
2014-08-03 13:51:06 -0400

    Lesson 3 complete

--- app/src/main/java/com/example/amitav/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/amitav/sunshine/DetailActivity.java
@@ -1,11 +1,11 @@
 package com.example.amitav.sunshine;
 
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
